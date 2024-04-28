70e0b02c00035c8fa49566a94dfca7bc13c474ef
choijh4161@gmail.com <21900764@handong.ac.kr>
2024-04-28 16:49:05 +0900

    init: 프로젝트 초기화

---
+++ raonz/src/main/java/hgu/se/raonz/RaonzApplication.java
@@ -0,0 +1,13 @@
+package hgu.se.raonz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaonzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaonzApplication.class, args);
+	}
+
+}
---
+++ raonz/src/test/java/hgu/se/raonz/RaonzApplicationTests.java
@@ -0,0 +1,13 @@
+package hgu.se.raonz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaonzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
