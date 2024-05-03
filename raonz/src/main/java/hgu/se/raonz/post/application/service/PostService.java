f3b6fcb1e0a9fd53b1e57a5784b0d0bacbc0fc25
김동규 <ehdrb01@gimdong-gyuui-MacBookPro.local>
2024-05-03 23:48:42 +0900

    feat: Post 삭제 및 업데이트 기능 구현

--- raonz/src/main/java/hgu/se/raonz/post/application/service/PostService.java
+++ raonz/src/main/java/hgu/se/raonz/post/application/service/PostService.java
@@ -3,6 +3,7 @@ package hgu.se.raonz.post.application.service;
 import hgu.se.raonz.post.domain.entity.Post;
 import hgu.se.raonz.post.domain.repository.PostRepository;
 import hgu.se.raonz.post.presentation.request.PostRequest;
+import hgu.se.raonz.post.presentation.request.PostUpdateRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,12 +22,32 @@ public class PostService {
         return post;
     }
 
-//    @Transactional
-//    public Post deletePost(Long postId) {
-//        Post post = postRepository.findPostByPostID(postId);
-////        post.getUser().deletePost(post);
-//        postRepository.delete(post);
-//
-//        return post;
-//    }
+    @Transactional
+    public Long deletePost(Long postId) {
+        Post post = postRepository.findById(postId).orElse(null);
+
+        if(post != null) {
+            Long returnId = post.getPostId();
+            postRepository.delete(post);
+
+            return returnId;
+        }
+
+        return null;
+    }
+
+    @Transactional
+    public Long updatePost(Long postId, PostUpdateRequest postUpdateRequest) {
+        Post post = postRepository.findById(postId).orElse(null);
+
+        if(post != null) {
+            post.setTitle(postUpdateRequest.getTitle());
+            post.setContent(postUpdateRequest.getContent());
+            postRepository.save(post);
+
+            return post.getPostId();
+        }
+
+        return null;
+    }
 }
--- raonz/src/main/java/hgu/se/raonz/post/domain/repository/PostRepository.java
+++ raonz/src/main/java/hgu/se/raonz/post/domain/repository/PostRepository.java
@@ -3,7 +3,7 @@ package hgu.se.raonz.post.domain.repository;
 import hgu.se.raonz.post.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, String> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
 //    Post findPostByPostID(Long postId);
 }
