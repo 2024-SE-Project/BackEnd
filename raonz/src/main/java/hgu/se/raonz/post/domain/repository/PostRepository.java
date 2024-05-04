12156d18931decb56902e932c4dc78477d379341
choijh4161@gmail.com <21900764@handong.ac.kr>
2024-05-04 13:31:32 +0900

    feat : 게시물 리스트 가져오기 기능 구현

--- raonz/src/main/java/hgu/se/raonz/post/domain/repository/PostRepository.java
+++ raonz/src/main/java/hgu/se/raonz/post/domain/repository/PostRepository.java
@@ -2,8 +2,13 @@ package hgu.se.raonz.post.domain.repository;
 
 import hgu.se.raonz.post.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-//    Post findPostByPostID(Long postId);
+    @Query("select r from Post r where r.type = :type")
+    List<Post> findPostListByType(@Param("type") int type);
 }
